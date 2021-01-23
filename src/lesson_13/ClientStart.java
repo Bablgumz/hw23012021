404543ad22dcbab6aa3fd939f646eeb592401c8d
Bablgumz <aleksandrsviridov824@gmail.com>
2021-01-23 16:44:14 +0300

    Здравствуйте! До конца не разобрался с этим заданием.

--- src/lesson_13/ClientStart.java
+++ src/lesson_13/ClientStart.java
@@ -1,8 +1,9 @@
 package lesson_13;
 
-public class Main {
+public class ClientStart {
 
     public static void main(String[] args) {
-	// write your code here
+        new Client3();
     }
 }
+
---
+++ src/lesson_13/ServerStart.java
@@ -0,0 +1,10 @@
+package lesson_13;
+
+public class ServerStart {
+    public static void main(String[] args) {
+        new Server3();
+    }
+}
+
+
+
